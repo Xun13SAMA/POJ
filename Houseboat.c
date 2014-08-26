3f9b266fe3b69a5bfbd73b1df46cfa6e5c4737e1
Xun13SAMA <xun13sama@gmail.com>
2014-08-26 23:00:21 +0800

    notac

---
+++ Houseboat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define PI 3.14
+#define DECAY 50
+int main()
+{
+	int counter;
+	scanf("%d",&counter);
+	printf("%d",counter);
+	int i;
+	float X, Y, area;
+	for(i = 0; i < counter; i++)
+	{
+		scanf("%f%f",&X,&Y);
+		area = (X * X + Y * Y) * PI / 2;
+		int year;
+		year  =	area / DECAY;
+		printf("Property %d: This Property will begin eroding in year %d\n",(i+1),year++);
+	}
+	return 0;
+}		
